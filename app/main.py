b2c7e4a807900943b87d74a947b5f8af3ea22460
Reza Maleknasab <farmailzad@gmail.com>
2024-08-21 18:21:41 +0330

    improved logic

--- app/main.py
+++ app/main.py
@@ -6,7 +6,6 @@
 # rate limiter
 # remove exceptions from return
 
-
 from decimal import Decimal
 from typing import Annotated, Optional
 from fastapi import FastAPI, Query
@@ -36,21 +35,42 @@ async def root(
 
             point = [long, lat]
 
-            print("point here:", point)
             return await get_near_points(point)
+
         except ValueError as ve:
             return {"status": "error", "message": f"Invalid latitude or longitude: {ve}"}
+        except Exception:
+            return {"status": "error", "message": "error fethcing locations using geopoint"}
 
     if ip:
-        point = await get_location_from_ip(ip)
-        return await get_near_points(point)
 
-    return await get_best_ads("nothing to look up on, recommending best ads")
+        point_response = await get_location_from_ip(ip)
+        if "error" in point_response:
+            return {"status": "error", "message": point_response.get("error")}
+        elif "non-local" in point_response:
+            message = point_response.get("non-local")
+            return {"status": "success", "message": f"{message}, recommending best ads" }
+
+        try:
+            for geo in point_response:
+                geo = float(geo)
+
+            return await get_near_points(point_response)
+
+        except ValueError as ve:
+            return {"status": "error", "message": f"Invalid latitude or longitude: {ve}"}
+        except Exception as e:
+            return {"status": "error", "message": "error fethcing locations using geopoint"}
+
+        return {"status": "success", "message": "no country detected, recommending best ads" }
+
+    return {"status": "success", "message": "nothing to look up on, recommending best ads" }
+    
+
+
 
 async def get_near_points(point):
 
-    if point == "outside range" or point == "no country detected":
-        return await get_best_ads(f"{point}, recommending best ads")
 
     try:
         objects = Advertisement.objects(location__near=point)
@@ -67,6 +87,9 @@ async def get_near_points(point):
 
 
 async def get_location_from_ip(ip_address):
+
+    # res types: error, non-local, lat and long
+
     try:
         url = f"http://ipwho.is/{ip_address}"
         response = await send_get_request(url)
@@ -75,18 +98,18 @@ async def get_location_from_ip(ip_address):
             return {"error": response['error']}
         
         elif 'country' in response and response.get('country').strip().lower() != "iran":
-            return "outside range"
+            return {"non-local": "country outside range"}
 
-        elif 'latitude' in response and 'longtitude' in response:
+        elif ('latitude' in response) and ('longitude' in response):
             return [response.get('longitude'), response.get('latitude')]
 
         else:
-            return "no country detected"
+            return {"non-local": "no country detected"}
         
     
     except Exception as e:
         logging.error(f"Error fetching location from IP: {e}")
-        return {"error": str(e)}
+        return {"error": "error fetching location via IP adress"}
 
 
 
@@ -108,7 +131,6 @@ async def send_get_request(url):
         return {"error": str(e)}
 
 
-async def get_best_ads(message):
-    return {"status": "success", "message": message}
+
 
 
